d051e64bfb48feade738b67e264f1bcf8daede9a
dariavie <fiji_li@mail.ru>
2021-09-25 15:48:16 +0300

    cafe

--- cafeProject/src/main/java/by/training/cafeproject/dao/pool/ConnectionPool.java
+++ cafeProject/src/main/java/by/training/cafeproject/dao/pool/ConnectionPool.java
@@ -1,7 +1,5 @@
 package by.training.cafeproject.dao.pool;
 
-import by.training.cafeproject.dao.impl.MySqlFactory;
-import com.mysql.cj.jdbc.MysqlDataSource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -9,13 +7,8 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 import java.util.ResourceBundle;
 import java.util.Set;
 import java.util.concurrent.BlockingQueue;
